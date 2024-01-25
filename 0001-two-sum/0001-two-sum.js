18b3ae4464a5259eeb5c2e9ef414402fb283f987
alejo86a <alejo86a@gmail.com>
2024-01-25 11:53:14 -0800

    Time: 201 ms (5.20%), Space: 55.9 MB (5.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,7 +6,6 @@
 var twoSum = function(nums, target) {
     for(let i=0;i<nums.length;i++){
         secondIndex = nums.toSpliced(i).findIndex((x)=>x===(target-nums[i]))
-      console.log(i,secondIndex)
         if(secondIndex!==-1){
             return [secondIndex,i]         
         }
