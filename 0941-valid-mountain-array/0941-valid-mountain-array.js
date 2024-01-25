6657ff1acd5cd76db0775589b527b3e71a424488
alejo86a <alejo86a@gmail.com>
2024-01-25 06:43:54 -0800

    Time: 44 ms (98.55%), Space: 51.8 MB (5.23%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+function validMountainArray(arr) {
+    if(arr.length < 3 || arr[0] > arr[1]) return false
+
+    var max_ind = 0
+    var growing = true
+    
+    for(let i = 1; i < arr.length; i++){
+        
+        if(growing && arr[i] > arr[max_ind]){
+            max_ind = i
+            if(arr[i-1] > arr[i] || max_ind + 1 === arr.length) return false
+        } else
+        {
+            growing = false
+            if(arr[i] <= arr[i+1] || arr[i] >= arr[max_ind]) return false
+        }
+    }
+    return true
+}
