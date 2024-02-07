6b4415f0f7acd8bff0007f9e7dafa61892bd457c
alejo86a <alejo86a@gmail.com>
2024-02-06 16:17:31 -0800

    Time: 72 ms (67.14%), Space: 54.1 MB (29.07%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    const arrMap = new Map();
+    for(let i = 0;i<s.length;i++){
+        if(arrMap.has(s[i])){
+            arrMap.set(s[i],(arrMap.get(s[i])+1))
+        }else{
+            arrMap.set(s[i],1)
+        }
+    }
+    return Array.from(arrMap).sort((a,b)=>b[1]-a[1]).map(x=>(new Array(x[1])).fill(x[0]).join('')).join('');
+};
