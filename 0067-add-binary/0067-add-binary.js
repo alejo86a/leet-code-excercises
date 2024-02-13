91d53f94df1406a91775e895cc79e84340976fbf
alejo86a <alejo86a@gmail.com>
2024-02-13 15:22:54 -0800

    Time: 66 ms (26.29%), Space: 51.2 MB (18.98%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    const l = a.length>=b.length?a.length:b.length;
+    let diff = 0;
+    const res = [];
+    for(let i =0;i<l;i++){
+        const val = ((i>=a.length?0:parseInt(a[a.length-1-i],2))+(i>=b.length?0:parseInt(b[b.length-1-i],2))+parseInt(diff)).toString(2);
+        res.unshift(val[val.length-1]);
+        diff = val.length>=2?1:0;
+    }
+    if(diff>0) res.unshift(diff);
+    return res.join('');
+};
+
+//return (parseInt(a,2)+parseInt(b,2)).toString(2);
